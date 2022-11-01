8136233d96ca6173eda2e403326a837035f0e53e
TomerSitton <tomer.sitton1@gmail.com>
2022-11-01 12:40:25 +0200

    added commands support!

--- ChannelMessages.py
+++ ChannelMessages.py
@@ -19,9 +19,9 @@ from telethon.tl.types import (
     MessageMediaPhoto
 )
 
-CATEGORIES = ["tools", "blogs","cve", "leak", "web", "windows persistance", "linux persistance", "windows pe", "linux pe"]
+#CATEGORIES = []
 COLUMNS = ["Date","Category", "Site Name","URL","Title","Description"]
-"""message.media.date, CATEGORY , message.media.site_name, message.media.display_url, message.media.title, message.media.description """
+FORBIDDEN_CHARS = [",", "'", "\"", "\\", "/", "{", "}", "[", "]", "`", "@", "#", "$", "%", "^", "*", ";", "?", "."]
 
 async def main(config):
     global CATEGORIES
@@ -58,25 +58,76 @@ async def main(config):
                 message_id = message.id
                 print("Current Message ID is:", message_id)
                 
-                if message.message.lower() in ["#" + category.lower() for category in CATEGORIES] and message.reply_to is not None:
-                        print("reply to message id: {}".format(message.reply_to.reply_to_msg_id))
-                        link_message = await client.get_messages(entity=my_channel, ids=message.reply_to.reply_to_msg_id)
-                        if link_message.media is not None:
-                            data = extract_message_data(link_message=link_message, hashtag_message=message)
-                            dump_data(data)
-                            await client.send_message(my_channel, 'Got it! Another link was added to the {} category in The Inventory!'.format(message.message.lower()))
+                
+                if message.message.lower().startswith("#") and message.reply_to is not None:
+                    link_message = await client.get_messages(entity=my_channel, ids=message.reply_to.reply_to_msg_id)
+                    await add_data_from_message(link_message=link_message, hashtag_message=message, channel=my_channel)
+
+                elif message.message.lower().startswith("/"):
+                    await handle_command(message.message, my_channel)
 
                 config.set('Telegram','message_id',str(message_id))
                 with open("config.ini","w") as config_file:
                     config.write(config_file)
-   
-def extract_message_data(link_message, hashtag_message):
+
+
+async def handle_command(message,channel):
+    global CATEGORIES
+    global FORBIDDEN_CHARS
+    command = message[1:].lower()
+    if len(command.split()) == 1:
+        if command in ["?","help","usage"]:
+            output = """Welcome to The Inventory!\nTo add a link, share it in the group and then reply to that message with #<Category>.\nTo list valid categories use the "/list" command.\nTo add a category, use the "/add <category>" command.\nTo remove a category, use the "/rm <category>" command."""
+        elif command in ["list", "ls", "show", "categories", "category"]:
+            output = "Valid categories are: {}".format("\n" + "\n".join(CATEGORIES))
+        else:
+            output = "Unknown command: {}".format(command)
+    elif len(command.split()) == 2:
+        cmd, param = command.split()
+        if True in [char in param for char in FORBIDDEN_CHARS]:
+            output = "Don't mess with me... INVALID PARAM!"
+        elif cmd in ["add"]:
+            if param in CATEGORIES:
+                output = "The requested category is already in The Inventory!"
+            else:
+                CATEGORIES.append(param)
+                config.set('Telegram','categories',",".join(CATEGORIES))
+                with open("config.ini","w") as config_file:
+                    config.write(config_file)
+                output = "Added category {} to The Inventory".format(param)
+        elif cmd in ["remove", "rm", "delete", "del"]:
+            if param not in CATEGORIES:
+                output = "The requested category is NOT in The Inventory!"
+            else:
+                CATEGORIES.remove(param)
+                config.set('Telegram','categories',",".join(CATEGORIES))
+                with open("config.ini","w") as config_file:
+                    config.write(config_file)
+                output = "Removed category {} from The Inventory".format(param)
+        else:
+            output = "Unknown command: {}".format(command)
+
+    await client.send_message(channel, output)
+
+async def add_data_from_message(link_message, hashtag_message,channel):
+    if link_message.media is not None:
+        data = await extract_message_data(link_message, hashtag_message, channel)
+        if data is not None:
+            dump_data(data)
+            await client.send_message(channel, 'Got it! Another link was added to the {} category in The Inventory!'.format(hashtag_message.message.lower()))
+
+
+async def extract_message_data(link_message, hashtag_message,channel):
     global CATEGORIES
     URL_EXTRACT_PATTERN = "https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)"
-    for category in CATEGORIES:
-        if hashtag_message.message.lower() == "#" + category.lower():
-            message_category = category
-            break
+    if hashtag_message.message.lower() in ["#" + category.lower() for category in CATEGORIES]:
+        for category in CATEGORIES:
+            if hashtag_message.message.lower() == "#" + category.lower():
+                message_category = category
+                break
+    else:
+        await client.send_message(channel, 'Unknown categury {}. Valid categories are: {}'.format(hashtag_message, "\n".join(CATEGORIES)))
+        return
     data = dict.fromkeys(COLUMNS)
     data["Date"] = link_message.date.strftime('%Y-%m-%d')
     data["Category"] =  message_category
@@ -125,6 +176,7 @@ if __name__ == '__main__':
     api_id = config['Telegram']['api_id']
     api_hash = config['Telegram']['api_hash']
     username = config['Telegram']['username']
+    CATEGORIES = config['Telegram']['categories'].split(",")
     client = TelegramClient(username, api_id, api_hash)
     with client:
         client.loop.run_until_complete(main(config))
